3d6af9b2f38c12518971ef7875e2ec504aff88ac
iradakhf <iradakhf@code.edu.az>
2022-07-06 23:56:22 +0400

    add all

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Car
+    {
+       public string brand;
+       public int price;
+       public int speed;
+        public string color;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercedes properties = new Mercedes();
+            properties.brand = "Mercedes";
+            Console.WriteLine(properties.brand);
+            BMW property = new BMW();
+            property.color = "red";
+            Console.WriteLine(property.color);
+
+        }
+    }
+}
